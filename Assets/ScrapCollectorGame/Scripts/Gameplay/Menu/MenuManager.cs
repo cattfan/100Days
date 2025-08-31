33c395459d83d85a32cf184089ed0ea6830da31c
KatHuyn <askerkiet@gmail.com>
2025-08-31 22:04:45 +0700

    menu cua thien

---
+++ Assets/ScrapCollectorGame/Scripts/Gameplay/Menu/MenuManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Maingame");
+        Time.timeScale = 1f;
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+
+        // Dòng này chỉ hoạt động khi game đã được build
+        Application.Quit();
+
+        // Dòng này dùng để dừng chạy game trong Unity Editor
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+
+    }
+}
